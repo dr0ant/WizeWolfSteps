56883522def82680d471977e8791db25fb1578ac
antoine larcher <44400727+dr0ant@users.noreply.github.com>
2023-12-28 16:32:59 +0100

    first map view

---
+++ script.js
@@ -0,0 +1,8 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'),{
+        center: {lat:-34.997, lng: 150.644},
+        zoom: 10,
+        mapId: 'f37f8b353c8cb73d'
+    });
+}
+
