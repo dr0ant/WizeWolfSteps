d752e58e37b149c7149fab3297b1b9aae8b8779c
antoine larcher <44400727+dr0ant@users.noreply.github.com>
2023-12-29 01:10:00 +0100

    truc qui marche vrmt enfaite

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
-from flask import Flask, render_template, jsonify
 import json
-import requests
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-def load_api_keys():
-    # Load API keys from the JSON file
-    with open('ApiKeys/GoogleAPIKEY.json', 'r') as file:
-        api_keys_data = json.load(file)
-    return api_keys_data
+def load_api_key_from_file():
+    try:
+        with open('ApiKeys/GoogleAPIKEY.json', 'r') as file:
+            api_key_data = json.load(file)
+        return api_key_data
+    except FileNotFoundError:
+        print("Error: ApiKeys/GoogleAPIKEY.json not found.")
+        return None
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/get_google_maps_link')
-def get_google_maps_link():
-    # Fetch the JSON data containing Maps API Key and Map IDs
-    try:
-        data = load_api_keys()
-        maps_api_key = data.get('Maps_API_Key')
-        print(maps_api_key)
-        maps_ids = data.get('Maps_ids')
-        print(maps_ids)
-        
-        # Construct the Google Maps API link
-        google_maps_api_link = f'https://maps.googleapis.com/maps/api/js?key={maps_api_key}&map_ids={maps_ids}&callback=initMap'
-        print(google_maps_api_link)
-        print(jsonify({'googleMapsApiLink': google_maps_api_link}))
-        return jsonify({'googleMapsApiLink': google_maps_api_link})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-      
+@app.route('/get_api_key')
+def get_api_key():
+    # Load the API key data from the file
+    api_key_data = load_api_key_from_file()
+
+    if api_key_data:
+        print("API Key Data:", api_key_data)  # Add this line for debugging
+        return jsonify(api_key_data)
+    else:
+        # Return an error response if the file is not found or cannot be read
+        return jsonify({'error': 'Unable to load API key data'})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
